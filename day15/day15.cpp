8d870098bae7e9f62bf3fd5716b9c9adcd991878
Jonathan Edwards <github@jonstuff.com>
2021-12-23 18:11:48 -0800

    day15 part2

--- day15/day15.cpp
+++ day15/day15.cpp
@@ -15,25 +15,23 @@ using namespace std::literals;
 struct Ingredient
 {
     std::string         name;
-    std::array<int,4>   properties;
+    std::array<int,5>   properties;
     
-    int         calories;
 };
 
 std::array<Ingredient,4> const ingredients
 {{
-    {"Sprinkles",    {2,   0,   -2,   0},     3 },
-    {"Butterscotch", {0,   5,   -3,   0},     3 },
-    {"Chocolate",    {0,   0,    5,  -1},     8 },
-    {"Candy",        {0,  -1,    0,   5},     8 },
+    {"Sprinkles",    {2,   0,   -2,   0,     3} },
+    {"Butterscotch", {0,   5,   -3,   0,     3} },
+    {"Chocolate",    {0,   0,    5,  -1,     8} },
+    {"Candy",        {0,  -1,    0,   5,     8} },
 }};
 
 
 struct Recipe
 {
     std::array<int,4>       amounts;
-    std::array<int,4>       properties;
-    std::array<int,4>       properties;
+    std::array<int,5>       properties;
     int                     score;
 
 };
@@ -57,7 +55,7 @@ try
                 recipe.properties={};
                 recipe.score     =1;                    
 
-                for(int property = 0; property<4;property++)
+                for(int property = 0; property<5;property++)
                 {
                     for(int ingredient=0;ingredient<4;ingredient++)
                     {
@@ -65,16 +63,30 @@ try
                     }
 
                     recipe.properties[property] = std::max(0, recipe.properties[property]);
+                }
+
+                recipe.score     =1;                    
+                for(int property=0;property<4;property++)       // product excluding calories
+                {
                     recipe.score*=recipe.properties[property];
                 }
+
                 recipes.push_back(recipe);
             }
         }
     }
 
     std::ranges::sort(recipes,{}, &Recipe::score);
-
     std::cout << std::format("part 1 : best score = {}\n",recipes.back().score);
+
+
+
+    auto [eraseStart, eraseEnd] = std::ranges::remove_if(recipes, [](auto recipe) { return recipe.properties[4]!=500;});
+
+    recipes.erase(eraseStart,eraseEnd);
+
+    std::cout << std::format("part 2 : best score = {} @ 500 calories\n",recipes.back().score);
+
 
     return 0;
 }
