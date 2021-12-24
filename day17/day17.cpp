286ab552f67ed6fc38514be4ff6cffdcddf8213c
Jonathan Edwards <github@jonstuff.com>
2021-12-23 20:39:38 -0800

    day 17 part 2

--- day17/day17.cpp
+++ day17/day17.cpp
@@ -18,41 +18,48 @@ using namespace std::literals;
 #include "include/stopwatch.h"
 
 constexpr std::array containers{43,3,4,10,21,44,4,6,47,41,34,17,17,44,36,31,46,9,27,38};
-constexpr uint32_t   target=150;
+constexpr uint32_t   targetVolume=150;
 
-void part1()
+void go()
 {
-    int matchingCombinations{};
+    std::array<int,21>  sizeCount{};            //   sizeCount[3]  is the number of combinations of 3 containers with a volume of 150
+
    
     // 20 : therefore ~1,000,000 combinations .  43ms
 
     Stopwatch sw;
 
-    for(uint32_t combination = 1; combination < 1<<containers.size(); combination++)
+    for(uint32_t combination = 1; combination < 1u<<containers.size(); combination++)
     {
-        uint32_t total{};
+        uint32_t volume{};
 
         for(int bit=0;bit < containers.size(); bit++)
         {
             if(combination & 1<<bit)
             {
-                total+=containers[bit];
+                volume+=containers[bit];
             }
         }
 
-        if(total==target)
+        if(volume==targetVolume)
         {
-            matchingCombinations++;
+            sizeCount[__popcnt(combination)]++;
         }
     }
 
-    std::cout << std::format("part 1 : {} in {}ms\n",matchingCombinations , sw.milliseconds());
+    std::cout << std::format("part 1 : {} in {}ms\n", std::accumulate(sizeCount.begin(), sizeCount.end(),0) , sw.milliseconds());
+
+    for(int i=0;i<sizeCount.size();i++)
+    {
+        std::cout << std::format("{:3} ways to use {:2} containers\n",sizeCount[i],i);
+    }
+
 }
 
 int main()
 try
 {
-    part1();
+    go();
 
 
     return 0;
